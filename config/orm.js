e93e84083a904081ad439b0a349a175d5ae55f91
Tyler Ward <T.ward26@gmail.com>
2019-05-21 12:39:58 -0500

    Orm.js working may be able to remove objToSql function

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,62 @@
-const connection = require("connection");
+const connection = require("./connection");
 
-//selectAll()
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
 
-//insertOne()
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
 
-//updateOne()
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
 
-//Export
+const orm = {
+    selectAll: function (table, cb) {
+        let queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table} (${cols}) VALUES (?);`;
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = `UPDATE ${table} SET `;
+        queryString += objToSql(objColVals);
+        // queryString += objColVals;
+        queryString += ` WHERE ${condition};`;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+//Export orm model
+module.exports = orm;
