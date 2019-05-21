0e35a5ea42f223e63882c468639e40681b229abb
Tyler Ward <T.ward26@gmail.com>
2019-05-21 12:44:51 -0500

    Burger.js file complete

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,22 @@
 const orm = require("../config/orm");
 
 //Call ORM functions using burger specific inputs
+const burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb){
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
+            cb(res);
+        });
+    }
+};
 
-//Export burger.js
+module.exports = burger;
